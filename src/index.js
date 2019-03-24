47acac34b8db2f4d019b53aea261d5c678b89621
SeanBecker <techtronixwebdesign@gmail.com>
2019-03-24 10:46:44 -0500

    added: node-fetch, path module, comments, mock data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import express from 'express';
+// express.js webserver module
+const express = require('express');
 
+// handles serverside fetch requests
+const fetch = require('node-fetch');
+
+// handles file paths
+const path = require('path');
+
+// invoke express app
 const app = express();
 
-const port = 1337;
+// host and port env variables and fallbacks
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 1337;
+
+
 
 app.get('/', (req, res) => {
   res.json('hello from the filtrify backend')
 })
 
-app.listen(1337)
+app.get('/filters', (req, res) => {
+  res.sendFile(path.join(__dirname, './_mock/', 'filters.json'));
+})
+
+app.listen(port);
