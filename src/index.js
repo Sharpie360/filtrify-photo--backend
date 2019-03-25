23421f229d84ffda6e6507cafb2c59c821f61d63
SeanBecker <techtronixwebdesign@gmail.com>
2019-03-24 23:58:56 -0500

    update: cors proxy running, no other routes open aon, swapped node-fetch for request

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 // enables cors for cross-stack comms
 const cors = require('cors');
 // handles serverside requests
-const fetch = require('node-fetch');
+const request = require('request');
 
 // handles file paths and mock data
 const path = require('path');
@@ -21,23 +21,33 @@ app.use(cors());
 app.use(logger);
 
 
-// root route
-app.get('/', (req, res) => {
-  res.json('hello from the filtrify backend');
+app.use('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+})
+
+// // root route
+// app.get('/', (req, res) => {
+//   res.json('hello from the filtrify backend');
+// });
+
+
+// // demo routes for mock data
+// app.get('/api/filters', (req, res) => {
+//   res.sendFile(path.join(__dirname, './_mock/', 'filters.json'));
+// });
+// app.get('/api/filters/:id', (req, res) => {
+//   const _id = req.params.id;
+//   res.json(_filterData[_id]);
+// });
+
+
+app.get('*', (req, res) => {
+  let url = req.path.substring(1);
+  console.log(url)
+  req.pipe(request.get(encodeURI(url))).pipe(res);
 });
-
-
-// demo routes for mock data
-app.get('/api/filters', (req, res) => {
-  res.sendFile(path.join(__dirname, './_mock/', 'filters.json'));
-});
-app.get('/api/filters/:id', (req, res) => {
-  const _id = req.params.id;
-  res.json(_filterData[_id]);
-});
-
-
-
 
 // host and port env variables and fallbacks
 const host = process.env.HOST || '0.0.0.0';
