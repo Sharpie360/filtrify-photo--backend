85e26aef14b7533deb8c92a2680ef49cb1ec3939
SeanBecker <techtronixwebdesign@gmail.com>
2019-03-25 20:35:39 -0500

    update: cleaned up code, removed commented out code, commented functionality, ready for merge

--- src/index.js
+++ src/index.js
@@ -1,50 +1,33 @@
 // express.js webserver module
 const express = require('express');
-// enables cors for cross-stack comms
+// cors modules
 const cors = require('cors');
 // handles serverside requests
 const request = require('request');
 // handles url validation @ joshuaferr1s
 const isValidUrl = require('url-validation');
 
-// handles file paths and mock data
-const path = require('path');
-const _filterData = require('./_mock/_filterData');
-
 // middlewares
 const logger = require('./middleware/logger');
-
-
 
 // express app
 const app = express();
 
+// add cors to api headers
+// enables cors for cross-stack comms
 app.use(cors());
+
+// request logger
 app.use(logger);
 
-
+// apply CORS headers to the respose 
 app.use('*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 })
 
-// // root route
-// app.get('/', (req, res) => {
-//   res.json('hello from the filtrify backend');
-// });
-
-
-// // demo routes for mock data
-// app.get('/api/filters', (req, res) => {
-//   res.sendFile(path.join(__dirname, './_mock/', 'filters.json'));
-// });
-// app.get('/api/filters/:id', (req, res) => {
-//   const _id = req.params.id;
-//   res.json(_filterData[_id]);
-// });
-
-
+// validate url and if true, finish request
 app.get('*', (req, res) => {
   let url = req.path.substring(1);
   isValidUrl(url) ? req.pipe(request.get(encodeURI(url))).pipe(res) : null;
@@ -54,4 +37,4 @@ app.get('*', (req, res) => {
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 1337;
 
-app.listen(port);
+app.listen(port, () => console.log(`Server started on port: ${port}`));
